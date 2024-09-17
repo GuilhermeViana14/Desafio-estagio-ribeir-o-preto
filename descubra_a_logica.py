f94156f220ef3ed1c358bb3ec1da169c930e23aa
GuilhermeViana14 <guilhermevianafilgueiras@gmail.com>
2024-09-17 13:27:16 -0300

    feito o codigo para descobrir a logica

--- descubra_a_logica.py
+++ descubra_a_logica.py
@@ -5,6 +5,30 @@ def elemento_a():
 
 
 def elemento_b():
-    sequencia = [2, 3, 5, 7]
-    proximo = sequencia[-1] + 2
+    sequencia = [2, 4, 8, 16, 32, 64]
+    proximo = sequencia[-1] * 2
+    return proximo
+
+
+def elemento_c():
+    sequencia = [0, 1, 4, 9, 16, 25, 36]
+    proximo = (len(sequencia)) ** 2
+    return proximo
+
+
+def elemento_d():
+    sequencia = [4, 16, 36, 64]
+    proximo = (len(sequencia)* 2 + 2 ) ** 2
+    return proximo
+
+
+def elemento_e():
+    sequencia = [1, 1, 2, 3, 5, 8]
+    proximo = sequencia[-1] + sequencia[-2]
+    return proximo
+
+
+def elemento_e():
+    sequencia = [2, 10, 12, 16, 17, 18, 19]
+    proximo = sequencia[-1] + 1
     return proximo
