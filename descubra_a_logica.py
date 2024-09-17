bc18e685e0cf49335c80cea1163fd6a516e94bc6
GuilhermeViana14 <guilhermevianafilgueiras@gmail.com>
2024-09-17 13:24:00 -0300

    criado o codigo para descobrir a logica

---
+++ descubra_a_logica.py
@@ -0,0 +1,10 @@
+def elemento_a():
+    sequencia = [1, 3, 5, 7]
+    proximo = sequencia[-1] + 2
+    return proximo
+
+
+def elemento_b():
+    sequencia = [2, 3, 5, 7]
+    proximo = sequencia[-1] + 2
+    return proximo
